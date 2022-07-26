1d769a04f2d0c2edfb4b538390a527749cba8263
Achmad Fauzi Harismawan <mail@harismawan.com>
2022-07-26 16:15:00 +0700

    APD-28 add http client class and few fix

--- test/client.test.js
+++ test/client.test.js
@@ -1,23 +1,23 @@
-const assert = require('assert')
 const bni = require('../index')
+const assert = require('assert')
 
 describe('client', () => {
   describe('getBaseUrl', () => {
     it(`should return https://newapidev.bni.co.id`, () => {
-      const client = new bni.Client({prod: false, client_id: '', client_secret: ''});
+      const client = new bni.Client({prod: false, clientId: '', clientSecret: ''});
       assert.strictEqual(client.getBaseUrl(), 'https://newapidev.bni.co.id');
     })
 
     it(`should return https://api.bni.co.id`, () => {
-      const client = new bni.Client({prod: true, client_id: '', client_secret: ''});
+      const client = new bni.Client({prod: true, clientId: '', clientSecret: ''});
       assert.strictEqual(client.getBaseUrl(), 'https://api.bni.co.id');
     })
   })
 
   describe('getConfig', () => {
-    it(`should return {prod: false, client_id: 'test12345', client_secret: 'test54321'}`, () => {
-      const client = new bni.Client({prod: false, client_id: 'test12345', client_secret: 'test54321'});
-      assert.deepEqual(client.getConfig(), {prod: false, client_id: 'test12345', client_secret: 'test54321'});
+    it(`should return {prod: false, clientId: 'test12345', clientSecret: 'test54321'}`, () => {
+      const client = new bni.Client({prod: false, clientId: 'test12345', clientSecret: 'test54321'});
+      assert.deepEqual(client.getConfig(), {prod: false, clientId: 'test12345', clientSecret: 'test54321'});
     })
   })
 })
---
+++ test/httpClient.test.js
@@ -0,0 +1,7 @@
+const HttpClient = require('../lib/httpClient');
+
+describe('httpClient', () => {
+  describe('request', () => {
+    // TODO TEST HERE
+  })
+})
