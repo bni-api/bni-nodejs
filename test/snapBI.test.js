5f2b78560d0260440a851f3f75e80cc829b20b59
Wawat Prigala <wawat.ganteng@gmail.com>
2022-10-05 17:51:45 +0700

    add negative case unit test snap bi

--- test/snapBI.test.js
+++ test/snapBI.test.js
@@ -7,9 +7,19 @@ describe('snapBI.js', () => {
 
   describe('getBalanceInquiry', () => {
 
-    it('should return responseCode 2000000', () => {
+    it('should return responseCode 2001100', () => {
       return getBalanceInquiry().then((res) => {
         assert.strictEqual(res.responseCode, '2001100');
+      });
+    });
+
+  });
+
+  describe('getBalanceInquiryFailed', () => {
+
+    it('should return responseCode 4001102', () => {
+      return getBalanceInquiryFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4001102');
       });
     });
 
@@ -25,11 +35,31 @@ describe('snapBI.js', () => {
 
   });
 
+  describe('getBankStatementFailed', () => {
+
+    it('should return responseCode 4001402', () => {
+      return getBankStatementFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4001402');
+      });
+    });
+
+  });
+
   describe('getInternalAccountInquiry', () => {
 
     it('should return responseCode 2001500', () => {
       return getInternalAccountInquiry().then((res) => {
         assert.strictEqual(res.responseCode, '2001500');
+      });
+    });
+
+  });
+
+  describe('getInternalAccountInquiryFailed', () => {
+
+    it('should return responseCode 4001502', () => {
+      return getInternalAccountInquiryFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4001502');
       });
     });
 
@@ -45,11 +75,31 @@ describe('snapBI.js', () => {
 
   });
 
+  describe('getTransactionStatusInquiryFailed', () => {
+
+    it('should return responseCode 4003602', () => {
+      return getTransactionStatusInquiryFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4003602');
+      });
+    });
+
+  });
+
   describe('getTransferIntraBank', () => {
 
     it('should return responseCode 2001700', () => {
       return getTransferIntraBank().then((res) => {
         assert.strictEqual(res.responseCode, '2001700');
+      });
+    });
+
+  });
+
+  describe('getTransferIntraBankFailed', () => {
+
+    it('should return responseCode 4001702', () => {
+      return getTransferIntraBankFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4001702');
       });
     });
 
@@ -65,11 +115,31 @@ describe('snapBI.js', () => {
 
   });
 
+  describe('getTransferRTGSFailed', () => {
+
+    it('should return responseCode 4002202', () => {
+      return getTransferRTGSFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4002202');
+      });
+    });
+
+  });
+
   describe('getTransferSKNBI', () => {
 
     it('should return responseCode 2002300', () => {
       return getTransferSKNBI().then((res) => {
         assert.strictEqual(res.responseCode, '2002300');
+      });
+    });
+
+  });
+
+  describe('getTransferSKNBIFailed', () => {
+
+    it('should return responseCode 4002302', () => {
+      return getTransferSKNBIFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4002302');
       });
     });
 
@@ -85,6 +155,16 @@ describe('snapBI.js', () => {
 
   });
 
+  describe('getExternalAccountInquiryFailed', () => {
+
+    it('should return responseCode 4001602', () => {
+      return getExternalAccountInquiryFailed().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4001602');
+      });
+    });
+
+  });
+
   describe('getTransferInterBank', () => {
 
     it('should return responseCode 2001800', () => {
@@ -93,6 +173,15 @@ describe('snapBI.js', () => {
       });
     });
 
+  });
+
+  describe('getTransferInterBankFailed', () => {
+
+    it('should return responseCode 4001802', () => {
+      return getTransferInterBank().then().catch((err) => {
+        assert.strictEqual(err.response.data.responseCode, '4001802');
+      });
+    });
   });
 
 });
@@ -115,11 +204,31 @@ const getBalanceInquiry = async () => {
   return res;
 };
 
+const getBalanceInquiryFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: credential.snap_bi.privateKey, channelId: credential.snap_bi.channelId });
+  const res = await snap.balanceInquiry({
+    partnerReferenceNo: '202010290000000000002ssss',
+    accountNo: '0115476117ssss'
+  });
+  return res;
+};
+
 const getBankStatement = async () => {
   const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
   const res = await snap.bankStatement({
     partnerReferenceNo: '202102102021',
     accountNo: '115233527',
+    fromDateTime: '2010-01-01T12:08:56+07:00',
+    toDateTime: '2011-01-01T12:08:56+07:00'
+  });
+  return res;
+};
+
+const getBankStatementFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.bankStatement({
+    partnerReferenceNo: '202102102021sssss',
+    accountNo: '115233527ssss',
     fromDateTime: '2010-01-01T12:08:56+07:00',
     toDateTime: '2011-01-01T12:08:56+07:00'
   });
@@ -136,6 +245,15 @@ const getInternalAccountInquiry = async () => {
   return res;
 };
 
+const getInternalAccountInquiryFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.internalAccountInquiry({
+    partnerReferenceNo: '202010290000000000002ssss',
+    beneficiaryAccountNo: '0115476151sssss'
+  });
+  return res;
+};
+
 const getTransactionStatusInquiry = async () => {
   const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
   const res = await snap.transactionStatusInquiry({
@@ -143,6 +261,26 @@ const getTransactionStatusInquiry = async () => {
     originalReferenceNo: '220531103343739748',
     originalExternalId: '20220531103340',
     serviceCode: '17',
+    transactionDate: '2022-05-31',
+    amount: {
+      value: '15000.00',
+      currency: 'IDR'
+    },
+    additionalInfo: {
+      deviceId: '123456',
+      channel: 'mobilephone'
+    }
+  });
+  return res;
+};
+
+const getTransactionStatusInquiryFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.transactionStatusInquiry({
+    originalPartnerReferenceNo: '202201911020000121ss',
+    originalReferenceNo: '220531103343739748ss',
+    originalExternalId: '20220531103340ss',
+    serviceCode: '17ss',
     transactionDate: '2022-05-31',
     amount: {
       value: '15000.00',
@@ -168,6 +306,30 @@ const getTransferIntraBank = async () => {
     beneficiaryEmail: '',
     currency: 'IDR',
     customerReference: '207113OO00842662',
+    feeType: 'OUR',
+    remark: 'DANA20220426170737356898YuliantoSariputra',
+    sourceAccountNo: '1000164314',
+    transactionDate: '2022-09-05T10:29:57+07:00',
+    additionalInfo: {
+      deviceId: '123456',
+      channel: 'mobilephone'
+    }
+  });
+  return res;
+};
+
+const getTransferIntraBankFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.transferIntraBank({
+    partnerReferenceNo: '207113OO00842662sss',
+    amount: {
+      value: '500000.00',
+      currency: 'IDR'
+    },
+    beneficiaryAccountNo: '1000161562ss',
+    beneficiaryEmail: '',
+    currency: 'IDR',
+    customerReference: '207113OO00842662ss',
     feeType: 'OUR',
     remark: 'DANA20220426170737356898YuliantoSariputra',
     sourceAccountNo: '1000164314',
@@ -215,6 +377,41 @@ const getTransferRTGS = async () => {
   return res;
 };
 
+const getTransferRTGSFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.transferRTGS({
+    partnerReferenceNo: '20220513095840015788857asd',
+    amount: {
+      value: '100000001.00',
+      currency: 'IDR'
+    },
+    beneficiaryAccountName: 'PTZomatoMediaIndonesia',
+    beneficiaryAccountNo: '3333333333ssss',
+    beneficiaryAccountAddress: 'JlGatotSubrotoNoKav18RW1KuninganBarKecMampangPrptKotaJakartaSelatanDaerahKhususIbukotaJakarta12710',
+    beneficiaryBankCode: 'CENAIDJA',
+    beneficiaryBankName: 'PTBANKCENTRALASIATbk',
+    beneficiaryCustomerResidence: '1',
+    beneficiaryCustomerType: '2',
+    beneficiaryEmail: '',
+    currency: 'IDR',
+    customerReference: '20220513095840015788857',
+    feeType: 'OUR',
+    kodePos: '-',
+    recieverPhone: '-',
+    remark: 'DANA20220513095840015788857PTZomatoMediaIndonesia',
+    senderCustomerResidence: '-',
+    senderCustomerType: '-',
+    senderPhone: '',
+    sourceAccountNo: '0115476151ss',
+    transactionDate: '2020-06-17T01:03:04+07:00',
+    additionalInfo: {
+      deviceId: '',
+      channel: ''
+    }
+  });
+  return res;
+};
+
 const getTransferSKNBI = async () => {
   const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
   const res = await snap.transferSKNBI({
@@ -250,12 +447,61 @@ const getTransferSKNBI = async () => {
   return res;
 };
 
+const getTransferSKNBIFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.transferSKNBI({
+    partnerReferenceNo: '20220523150428586179325ss',
+    amount: {
+      value: '10000001.00',
+      currency: 'IDR'
+    },
+    beneficiaryAccountName: 'PTZomatoMediaIndonesia',
+    beneficiaryAccountNo: '0115476117sss',
+    beneficiaryAddress: 'JlGatotSubrotoNoKav18RW1KuninganBarKecMampangPrptKotaJakartaSelatanDaerahKhususIbukotaJakarta12710',
+    beneficiaryBankCode: 'CENAIDJAXXX',
+    beneficiaryBankName: 'PTBANKCENTRALASIATbk',
+    beneficiaryCustomerResidence: '1',
+    beneficiaryCustomerType: '2',
+    beneficiaryEmail: '',
+    currency: 'IDR',
+    customerReference: '20220523150428586179325ss',
+    feeType: 'OUR',
+    kodePos: '',
+    recieverPhone: '',
+    remark: 'DANA20220523150428586179325PTZomatoMediaIndonesia',
+    senderCustomerResidence: '',
+    senderCustomerType: '',
+    senderPhone: '',
+    sourceAccountNo: '0115476151',
+    transactionDate: '2020-06-17T01:03:04+07:00',
+    additionalInfo: {
+      deviceId: '',
+      channel: ''
+    }
+  });
+  return res;
+};
+
 const getExternalAccountInquiry = async () => {
   const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
   const res = await snap.externalAccountInquiry({
     beneficiaryBankCode: '002',
     beneficiaryAccountNo: '888801000157508',
     partnerReferenceNo: '2020102900000000000001',
+    additionalInfo: {
+      deviceId: '123456',
+      channel: 'mobilephone'
+    }
+  });
+  return res;
+};
+
+const getExternalAccountInquiryFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.externalAccountInquiry({
+    beneficiaryBankCode: '002',
+    beneficiaryAccountNo: '888801000157508ss',
+    partnerReferenceNo: '2020102900000000000001ss',
     additionalInfo: {
       deviceId: '123456',
       channel: 'mobilephone'
@@ -274,6 +520,33 @@ const getTransferInterBank = async () => {
     },
     beneficiaryAccountName: 'BONIFASIUSPRIOKO',
     beneficiaryAccountNo: '3333333333',
+    beneficiaryAddress: 'Palembang',
+    beneficiaryBankCode: '014',
+    beneficiaryBankName: 'BCA',
+    beneficiaryEmail: 'yories.yolanda@work.bri.co.id',
+    currency: 'IDR',
+    customerReference: '10052025',
+    sourceAccountNo: '0115476151',
+    transactionDate: '2022-06-14T12:08:56+07:00',
+    feeType: 'OUR',
+    additionalInfo: {
+      deviceId: '12345679237',
+      channel: 'mobilephone'
+    }
+  });
+  return res;
+};
+
+const getTransferInterBankFailed = async () => {
+  const snap = new SnapBI(client, { privateKeyPath: './private.key', channelId: '95221' });
+  const res = await snap.transferInterBank({
+    partnerReferenceNo: '2022090254000000000021ss',
+    amount: {
+      value: '55000',
+      currency: 'IDR'
+    },
+    beneficiaryAccountName: 'BONIFASIUSPRIOKO',
+    beneficiaryAccountNo: '3333333333ss',
     beneficiaryAddress: 'Palembang',
     beneficiaryBankCode: '014',
     beneficiaryBankName: 'BCA',
